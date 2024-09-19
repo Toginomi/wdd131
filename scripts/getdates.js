380ee85d319eeb1e042b78b80445e52534eb7f70
Toginomi <rudyiipilande@gmail.com>
2024-09-19 13:43:43 +0800

    home page

---
+++ scripts/getdates.js
@@ -0,0 +1,7 @@
+const yearElement = document.getElementById('currentyear');
+const currentYear = new Date().getFullYear();
+yearElement.textContent = currentYear;
+
+const lastModifiedElement = document.getElementById('lastModified');
+const lastModifiedDate = document.lastModified;
+lastModifiedElement.textContent = `Last modified: ${lastModifiedDate}`;
